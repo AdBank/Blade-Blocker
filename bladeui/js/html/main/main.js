876f5844f118be1cafd1b2e9eff00c20c7d1f670
Max Dyachenko <maksym.dyachenko@axondegroup.com>
2019-05-31 17:23:59 +0300

    NEW: AD-1880 - add second

--- bladeui/js/html/main/main.js
+++ bladeui/js/html/main/main.js
@@ -2,19 +2,21 @@
 
 "use strict";
 
+const checkbox = require("../common/checkbox");
+
 const html = `
 <div class="main-view flex-column">
   <div class="logo">
     <img src="./skin/blade_assets/blade-powered-adbank-logo.svg" 
     alt="blade logo" />
   </div>
-  <p class="content">
-    <p class="count">123</p>
-    <p class="caption">CURRENT BLOCKS</p>
-  </p>
+  <div class="content">
+    <p class="count" id="blockedCount"></p>
+    <p class="caption-count">CURRENT BLOCKS</p>
+  </div>
   <footer>
-    <button class="main-action-button" id="action-btn">DOWNLOAD NOW</button>
-    <p class="caption">Learn why you need two extensions here.</p>
+    <div class="version"><p class="text">Version</p><p class="version-number">0.0.1</p></div>
+    <div class="toggler" id="toggler"><p>Status</p>${checkbox()}</div>
   </footer>
 </div>
 `;
--- bladeui/js/pages/main/main.js
+++ bladeui/js/pages/main/main.js
@@ -3,6 +3,7 @@
 "use strict";
 
 const BaseClass = require("../common/baseClass");
+const {isPageWhitelisted} = require("../../popup.utils.js");
 
 class Main extends BaseClass
 {
@@ -15,7 +16,51 @@ class Main extends BaseClass
   {
     const mainAppWrapper = document.getElementById("main-app-wrapper");
 
+    this.toggler = document.getElementById("checkbox");
+    const blockedCount = document.getElementById("blockedCount");
+
+    browser.tabs.query({active: true, lastFocusedWindow: true}, tabs =>
+    {
+      if (tabs.length)
+      {
+        this.initToggleOnOff({id: tabs[0].id, url: tabs[0].url})
+      };
+    });
+
+    browser.runtime.sendMessage({type: "prefs.get", key: "blocked_total"}, (blockedTotal) => {
+      blockedCount.innerHTML = blockedTotal;
+    });
+
     mainAppWrapper.classList.remove("custom-bg");
+  }
+
+  initToggleOnOff(tab)
+  {
+    isPageWhitelisted(tab, whitelisted =>
+    {
+      if (whitelisted)
+      {
+        this.toggler.checked = false;
+      }
+    });
+
+    this.toggler.addEventListener("change", () =>
+    {
+      if (this.toggler.checked)
+      {
+        browser.runtime.sendMessage({
+          type: "filters.unwhitelist",
+          tab
+        });
+      }
+      else
+      {
+        browser.runtime.sendMessage({
+          type: "filters.whitelist",
+          tab
+        });
+      }
+    });
   }
 }
 
