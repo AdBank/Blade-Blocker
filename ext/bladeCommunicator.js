26d041b51aa338df6067bf9551d2d76f81a19beb
Vlad Khitev <vlad.khitev@axondevgroup.com>
2020-02-18 11:28:26 +0200

    Fix chrome whitelist issue

--- ext/bladeCommunicator.js
+++ ext/bladeCommunicator.js
@@ -12,8 +12,22 @@ browser.runtime.onMessageExternal.addListener(
     }
 
     // listener on message when replacer sends user bladeid (after user signin/signup)
-    if (request.userBladeId)
-    {
+    if (request.userBladeId) {
       sendResponse({userBladeIdReceived: true});
+
+      browser.tabs.query({active: true, lastFocusedWindow: true})
+        .then(tabs => {
+          if (tabs.length > 0) {
+            browser.runtime.sendMessage({
+              type: "filters.whitelist",
+              tab: {id: tabs[0].id, url: tabs[0].url}
+            }).then(() => {
+              browser.runtime.sendMessage({
+                type: "filters.unwhitelist",
+                tab: {id: tabs[0].id, url: tabs[0].url}
+              });
+            });
+          }
+        });
     }
   });
